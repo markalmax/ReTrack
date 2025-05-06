1a69236cc424aadafb49f956f785b79359d26727
markalmax <markalmax782@gmail.com>
2025-05-07 00:43:29 +0300

    added basic flutter boilerplate

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const StartPage());
+}
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
