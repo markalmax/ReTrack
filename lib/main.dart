0c645e3b02acf25aafb8463f2c35d85828dc727b
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-05-08 17:31:34 +0300

    added basic Bottom NavBar (subject to heavy change)

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +20,27 @@ class _StartPageState extends State<StartPage> {
       theme: ThemeData.from(colorScheme: const ColorScheme.light()),
       darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark()),
       themeMode: ThemeMode.dark,
-      home: Scaffold(),
+      home: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.music_note), label: 'Music'),
+            NavigationDestination(icon: Icon(Icons.album), label: 'Albums'),
+            NavigationDestination(
+              icon: Icon(Icons.queue_music),
+              label: 'Playlists',
+            ),
+          ],
+          selectedIndex: selectedIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+        ),
+        body:
+            [Container(), Container(), Container(), Container()][selectedIndex],
+      ),
     );
   }
 }
