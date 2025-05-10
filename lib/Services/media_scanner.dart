1939e3202a412ae68e6f3ca63dfd92a3723c243e
markalmax <markalmax782@gmail.com>
2025-05-10 16:09:56 +0300

    fixed permissions bug with android 12 and under

--- lib/Services/media_scanner.dart
+++ lib/Services/media_scanner.dart
@@ -1,11 +1,10 @@
-import 'dart:io';
-
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class MediaScanner {
   Future<List<SongModel>?> getAllSongs() async {
-    if (await Permission.audio.request().isGranted) {
+    if (await Permission.audio.request().isGranted ||
+        await Permission.storage.request().isGranted) {
       final OnAudioQuery query = OnAudioQuery();
       return await query.querySongs();
     } else {
