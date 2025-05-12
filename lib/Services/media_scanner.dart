79ba1016500593314b9fae0b6b6f423c1e6ed05c
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-05-13 02:14:34 +0300

    added IOS Permission requests
    Still not functional, Would have to figure out how the media library
    works on IOS

--- lib/Services/media_scanner.dart
+++ lib/Services/media_scanner.dart
@@ -1,10 +1,21 @@
+import 'dart:io' show Platform;
+
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class MediaScanner {
   Future<List<SongModel>?> getAllSongs() async {
-    if (await Permission.audio.request().isGranted ||
-        await Permission.storage.request().isGranted) {
+    bool isAllowed = false;
+
+    if (Platform.isAndroid) {
+      isAllowed =
+          await Permission.audio.request().isGranted ||
+          await Permission.storage.request().isGranted;
+    } else {
+      isAllowed = await Permission.mediaLibrary.request().isGranted;
+    }
+
+    if (isAllowed) {
       final OnAudioQuery query = OnAudioQuery();
       return await query.querySongs();
     } else {
