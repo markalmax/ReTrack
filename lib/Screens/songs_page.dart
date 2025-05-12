850c0da78593897c5b60719980d33d592b10f88b
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-05-12 03:06:24 +0300

    removed padding arround songs page
    It just made things look crammed
    SafeArea is still there tho :>

--- lib/Screens/songs_page.dart
+++ lib/Screens/songs_page.dart
@@ -26,114 +26,111 @@ class _SongsPageState extends State<SongsPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: FutureBuilder(
-          future: _list,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              songs = snapshot.data;
-              if (songs != null && songs!.isNotEmpty) {
-                return Column(
-                  children: [
-                    SearchAnchor(
-                      builder: (context, controller) {
-                        return SearchBar(
-                          controller: controller,
-                          onTap: () {
-                            controller.openView();
-                          },
-                          onChanged: (_) {
-                            controller.openView();
-                          },
-                          leading: const Icon(Icons.search),
-                        );
-                      },
-                      suggestionsBuilder: (context, controller) {
-                        final text = controller.text.toLowerCase();
-                        final result =
-                            songs!
-                                .where(
-                                  (song) =>
-                                      song.title.toLowerCase().contains(text),
-                                )
-                                .toList();
-                        return result.map(
-                          (song) => ListTile(
-                            title: Text(song.title),
-                            leading: SizedBox(
-                              width: 50,
-                              child: QueryArtworkWidget(
-                                id: song.id,
-                                artworkBorder: BorderRadius.circular(7),
-                                type: ArtworkType.AUDIO,
-                                nullArtworkWidget: Icon(Icons.music_note),
-                              ),
-                            ),
-                            onTap: () => controller.closeView(song.title),
-                          ),
-                        );
-                      },
-                    ),
-                    Expanded(
-                      child: ListView.builder(
-                        scrollDirection: Axis.vertical,
-                        shrinkWrap: true,
-                        itemCount: songs!.length,
-                        itemBuilder: (_, index) {
-                          return ListTile(
-                            leading: SizedBox(
-                              width: 50,
-                              child: QueryArtworkWidget(
-                                id: songs![index].id,
-                                artworkBorder: BorderRadius.circular(7),
-                                type: ArtworkType.AUDIO,
-                                nullArtworkWidget: Icon(Icons.music_note),
-                              ),
-                            ),
-                            trailing: PopupMenuButton(
-                              onSelected: (value) {
-                                switch (value) {
-                                  case "details":
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder:
-                                            (context) => SongDetailsPage(
-                                              song: songs![index],
-                                            ),
-                                      ),
-                                    );
-                                  default:
-                                }
-                              },
-                              itemBuilder:
-                                  (context) => <PopupMenuItem<String>>[
-                                    const PopupMenuItem(
-                                      value: "details",
-                                      child: Text("Details"),
-                                    ),
-                                  ],
-                            ),
-                            title: Text(songs![index].title),
-                          );
+      child: FutureBuilder(
+        future: _list,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            songs = snapshot.data;
+            if (songs != null && songs!.isNotEmpty) {
+              return Column(
+                children: [
+                  SearchAnchor(
+                    builder: (context, controller) {
+                      return SearchBar(
+                        controller: controller,
+                        onTap: () {
+                          controller.openView();
                         },
-                      ),
+                        onChanged: (_) {
+                          controller.openView();
+                        },
+                        leading: const Icon(Icons.search),
+                      );
+                    },
+                    suggestionsBuilder: (context, controller) {
+                      final text = controller.text.toLowerCase();
+                      final result =
+                          songs!
+                              .where(
+                                (song) =>
+                                    song.title.toLowerCase().contains(text),
+                              )
+                              .toList();
+                      return result.map(
+                        (song) => ListTile(
+                          title: Text(song.title),
+                          leading: SizedBox(
+                            width: 50,
+                            child: QueryArtworkWidget(
+                              id: song.id,
+                              artworkBorder: BorderRadius.circular(7),
+                              type: ArtworkType.AUDIO,
+                              nullArtworkWidget: Icon(Icons.music_note),
+                            ),
+                          ),
+                          onTap: () => controller.closeView(song.title),
+                        ),
+                      );
+                    },
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: songs!.length,
+                      itemBuilder: (_, index) {
+                        return ListTile(
+                          leading: SizedBox(
+                            width: 50,
+                            child: QueryArtworkWidget(
+                              id: songs![index].id,
+                              artworkBorder: BorderRadius.circular(7),
+                              type: ArtworkType.AUDIO,
+                              nullArtworkWidget: Icon(Icons.music_note),
+                            ),
+                          ),
+                          trailing: PopupMenuButton(
+                            onSelected: (value) {
+                              switch (value) {
+                                case "details":
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder:
+                                          (context) => SongDetailsPage(
+                                            song: songs![index],
+                                          ),
+                                    ),
+                                  );
+                                default:
+                              }
+                            },
+                            itemBuilder:
+                                (context) => <PopupMenuItem<String>>[
+                                  const PopupMenuItem(
+                                    value: "details",
+                                    child: Text("Details"),
+                                  ),
+                                ],
+                          ),
+                          title: Text(songs![index].title),
+                        );
+                      },
                     ),
-                  ],
-                );
-              } else {
-                print("data: ");
-                print(snapshot.data);
-                return Text("Sorry");
-              }
+                  ),
+                ],
+              );
             } else {
-              print("Data2: ");
+              print("data: ");
               print(snapshot.data);
               return Text("Sorry");
             }
-          },
-        ),
+          } else {
+            print("Data2: ");
+            print(snapshot.data);
+            return Text("Sorry");
+          }
+        },
       ),
     );
   }
