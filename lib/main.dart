86fc6c1fbeae4de53cf48072a94055c283b45799
Felo <68559765+Felo1000110@users.noreply.github.com>
2025-06-03 17:52:20 +0300

    removed appbar for now
    it looked ugly

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,11 @@ import 'package:retrack/Screens/songs_page.dart';
 import 'package:retrack/Screens/playlist_page.dart';
 
 void main() {
-  runApp(StartPage());
+  runApp(const StartPage());
 }
 
 class StartPage extends StatefulWidget {
-  StartPage({super.key});
-  final Map<int, String> pageNames = {
-    0: 'Home',
-    1: 'Music',
-    2: 'Albums',
-    3: 'Playlists',
-  };
+  const StartPage({super.key});
 
   @override
   State<StartPage> createState() => _StartPageState();
@@ -29,10 +23,6 @@ class _StartPageState extends State<StartPage> {
       darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark()),
       themeMode: ThemeMode.dark,
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(widget.pageNames[selectedIndex]!),
-        ),
         bottomNavigationBar: NavigationBar(
           destinations: [
             NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
